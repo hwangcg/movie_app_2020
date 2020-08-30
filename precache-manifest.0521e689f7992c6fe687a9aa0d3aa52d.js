4d29e73de348c4ab7ec473062fc077d57f379c59
HJK <hwangcz87@gmail.com>
2020-08-30 20:46:32 +0900

    Updates

---
+++ precache-manifest.0521e689f7992c6fe687a9aa0d3aa52d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560bed21f2f3c832aa06b7ceee0a4a51",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "76c6cb6d8f316755aa0b",
+    "url": "/movie_app_2020/static/css/main.c90680cc.chunk.css"
+  },
+  {
+    "revision": "d9d1c5872b231845d6c4",
+    "url": "/movie_app_2020/static/js/2.41e557d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.41e557d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c6cb6d8f316755aa0b",
+    "url": "/movie_app_2020/static/js/main.3f4c5b72.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
